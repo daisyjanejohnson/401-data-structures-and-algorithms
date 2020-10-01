bec84007e9c35e3b4c5236177394bb58f0c2215a
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-09-30 23:50:17 -0600

    direct flight finished

--- Data-Structures/graph/graph.js
+++ Data-Structures/graph/graph.js
@@ -4,14 +4,12 @@ const { Queue } = require('../stacks-and-queues/queue');
 class Node {
   constructor(value) {
     this.value = value;
-    this.neighbors = [];
   }
 }
 // EDGE is the relationship between two nodes.
 class Edge {
-  constructor(origin, destination, weight) {
-    this.origin = origin;
-    this.destination = destination;
+  constructor(node, weight) {
+    this.node = node;
     this.weight = weight;
 
   }
@@ -19,29 +17,33 @@ class Edge {
 
 class Graph {
   constructor() {
-    this.adjacencyList = new Set();
-    this.edges = [];
+    this.adjacencyList = new Map();
   }
 
   // Adds a new node to the graph
   // Takes in the value of that node
   // Returns the added node
   addNode(node) {
-    let newNode = new Node(node.value);
-    this.adjacencyList.add(newNode);
-    return newNode;
+    this.adjacencyList.set(node, []);
   }
 
   //  Adds a new edge between two nodes in the graph
   // Include the ability to have a “weight”
   // Takes in the two nodes to be connected by the edge
   // Both nodes should already be in the Graph
-  addEdge(origin, destination, weight=null) {
-    origin.neighbors.push(destination.value);
-    destination.neighbors.push(origin.value);
+  addEdge(origin, destination, weight = 0) {
+    if(!this.adjacencyList.has(origin)){
+      return;
+    }
+    if(!this.adjacencyList.has(destination)){
+      return;
+    }
 
-    const edge = new Edge(origin, destination, weight);
-    this.edges.push(edge);
+    const adjacencies = this.adjacencyList.get(origin);
+
+    const edge = new Edge(destination, weight);
+
+    adjacencies.push(edge);
     }
   //  Returns all of the nodes in the graph as a collection (set, list, or similar)
   getNodes() {
@@ -51,14 +53,7 @@ class Graph {
   // Takes in a given node
   // Include the weight of the connection in the returned collection
   getNeighbors(node) {
-    const myEdges = [];
-
-    this.edges.forEach(edge => {
-      if(edge.origin.value === node.value){
-        myEdges.push(edge);
-      }
-    });
-    return myEdges;
+    return this.adjacencyList.get(node);
   }
   //  Returns the total number of nodes in the graph
   size() {
